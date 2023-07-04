42da7c895986462a8e1b9147ce89b536605f5382
qybing <qiaoyanbing1@163.com>
2023-07-04 23:10:48 +0800

    更新voc转换成yolo格式

---
+++ voc2yolo/__init__.py
@@ -0,0 +1,6 @@
+#! python3
+# _*_ coding: utf-8 _*_
+# @Time : 2023/7/4 22:10 
+# @Author : Jovan
+# @File : __init__.py.py
+# @desc :
---
+++ voc2yolo/split_data.py
@@ -0,0 +1,35 @@
+import os
+import random
+
+trainval_percent = 1
+train_percent = 0.9
+xmlfilepath = 'Annotations'
+txtsavepath = 'ImageSets/Main'
+total_xml = os.listdir(xmlfilepath)
+
+num = len(total_xml)
+list = range(num)
+tv = int(num * trainval_percent)
+tr = int(tv * train_percent)
+trainval = random.sample(list, tv)
+train = random.sample(trainval, tr)
+ftrainval = open('ImageSets/Main/trainval.txt', 'w')
+ftest = open('ImageSets/Main/test.txt', 'w')
+ftrain = open('ImageSets/Main/train.txt', 'w')
+fval = open('ImageSets/Main/val.txt', 'w')
+print("sum:{},ftrainval:{},ftrain:{}, fval:{}, ftest:{}".format(num, len(trainval), len(train), len(trainval)-len(train), num-len(trainval)))
+for i in list:
+    name = total_xml[i][:-4] + '\n'
+    if i in trainval:
+        ftrainval.write(name)
+        if i in train:
+            ftrain.write(name)
+        else:
+            fval.write(name)
+    else:
+        ftest.write(name)
+
+ftrainval.close()
+ftrain.close()
+fval.close()
+ftest.close()
