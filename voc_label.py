9f2df9daeabebd16e5a92a9dc67e8a3846690b1e
Shadow <qiaoyanbing6@gmail.com>
2025-06-24 15:49:50 +0800

    Update voc_label.py
    增加类别数量的功能

--- voc_label.py
+++ voc_label.py
@@ -1,7 +1,8 @@
-import chardet
-from lxml import etree
 import shutil
 import os
+
+import chardet
+from lxml import etree
 
 
 def convert(size, box):
@@ -25,14 +26,7 @@ def detect_encoding(file_path):
         return result['encoding']
 
 
-def get_images_id(txt_path):
-    images_id = []
-    with open(txt_path, 'r') as f:
-        for line in f.readlines():
-            images_id.append(line.strip())
-    return images_id
-
-def convert_annotation(image_id,ann_path,label_path):
+def convert_annotation(image_id, ann_path, label_path,class_count):
     '''
     生成txt文件
     :param image_id:文件名称(不带后缀)
@@ -42,32 +36,41 @@ def convert_annotation(image_id,ann_path,label_path):
     '''
     ann_name = image_id + '.xml'
     txt_name = image_id + '.txt'
-    out_file = open(f'{os.path.join(label_path,txt_name)}', 'w')
+    # in_file = open(f'{os.path.join(ann_path,ann_name)}',encoding='utf-8')
+    out_file = open(f'{os.path.join(label_path, txt_name)}', 'w')
     try:
         encoding = detect_encoding(os.path.join(ann_path, ann_name))
-        tree = etree.parse(os.path.join(ann_path,ann_name),parser=etree.XMLParser(encoding=encoding))
+        tree = etree.parse(os.path.join(ann_path, ann_name), parser=etree.XMLParser(encoding=encoding))
         root = tree.getroot()
     except Exception as e:
-        print(f"file:{os.path.join(ann_path,ann_name)},error:{e}")
-    size = root.find('size')
-    w = int(size.find('width').text)
-    h = int(size.find('height').text)
-    for obj in root.iter('object'):
-        try:
-            difficult = obj.find('difficult').text
-        except Exception as e:
-            print(f"{e},name:{ann_name}")
-            difficult = 0
-        cls = obj.find('name').text
-        if cls not in classes or int(difficult) == 1:
-            continue
-        cls_id = classes.index(cls)
-        xmlbox = obj.find('bndbox')
-        b = (float(xmlbox.find('xmin').text), float(xmlbox.find('xmax').text), float(xmlbox.find('ymin').text),
-             float(xmlbox.find('ymax').text))
-        bb = convert((w, h), b)
-        out_file.write(str(cls_id) + " " + " ".join([str(a) for a in bb]) + '\n')
+        print(f"file:{os.path.join(ann_path, ann_name)},error:{e}")
+    try:
+        size = root.find('size')
+        w = int(size.find('width').text)
+        h = int(size.find('height').text)
+        for obj in root.iter('object'):
+            try:
+                difficult = obj.find('difficult').text
+            except Exception as e:
+                difficult = '0'
+                # print(f"difficult: {e},{ann_name}")
+            cls = obj.find('name').text
+            if cls not in classes or int(difficult) == 1:
+                continue
+            cls_id = classes.index(cls)
+            xmlbox = obj.find('bndbox')
+            b = (float(xmlbox.find('xmin').text), float(xmlbox.find('xmax').text), float(xmlbox.find('ymin').text),
+                 float(xmlbox.find('ymax').text))
+            bb = convert((w, h), b)
+            out_file.write(str(cls_id) + " " + " ".join([str(a) for a in bb]) + '\n')
+            if classes not in class_count:
+                class_count[classes] = 1
+            else:
+                class_count[classes] += 1
+    except Exception as e:
+        print(f"file:{os.path.join(ann_path, ann_name)},name:{root.find('filename').text}, error:{e}")
     return root.find('filename').text
+
 
 def copy_image(src_img_path, dst_img_path):
     '''
@@ -79,39 +82,64 @@ def copy_image(src_img_path, dst_img_path):
     src_img_list = os.listdir(src_img_path)
     for src_img in src_img_list:
         old_path = os.path.join(src_img_path, src_img)
-        new_path = os.path.join(dst_img_path,src_img)
-        shutil.copyfile(old_path,new_path)
+        new_path = os.path.join(dst_img_path, src_img)
+        shutil.copyfile(old_path, new_path)
     print(f'count:{len(src_img_list)},copy finish')
+
+
+def get_images_id(txt_path):
+    images_id = []
+    with open(txt_path, 'r', encoding='utf-8') as f:
+        for line in f.readlines():
+            images_id.append(line.strip())
+    return images_id
+
 
 # yolo需要的文件地址
 current_dir = os.getcwd()
+# current_dir = current_dir.replace('bag0228','bag0226')
 label_path = r'labels'
 dst_img_path = 'images'
 
-src_img_path = 'img'
+src_img_path = 'JPEGImages'
 # 切分好的train.txt的位置
 train_path = r'ImageSets/Main'
-ann_path = r'xml'
+ann_path = r'Annotations'
 sets = ['train', 'val', 'test']
+img_dict = {}
+img_list = os.listdir(src_img_path)
+for img_name in img_list:
+    img_name_list = os.path.splitext(img_name)
+    img_id = img_name_list[0]
+    img_dict[img_id] = img_name
+
 # 类别
-classes = ['']
+classes = ['poster', 'edible oil', 'pot', 'barreled laundry detergent', 'dishwashing liquid', 'tissue paper', 'roll type paper', 'smart screen',
+           'gun type camera', 'audio system', 'tent', 'bucket', 'router', 'electronic blood pressure monitor',
+           'table', 'bags laundry detergent', 'packaging box', 'banner', 'rice', 'dome camera', 'towel','trash can']
 print(f"classes:{len(classes)}")
 if not os.path.exists(label_path):
     os.makedirs(label_path)
 if not os.path.exists(dst_img_path):
     os.makedirs(dst_img_path)
-copy_image(src_img_path,dst_img_path)
-
+copy_image(src_img_path, dst_img_path)
+class_count ={}
 for image_set in sets:
     # image_ids = open(os.path.join(train_path, image_set + '.txt')).read().strip().split()
     image_ids = get_images_id(os.path.join(train_path, image_set + '.txt'))
     list_file = open('%s.txt' % (image_set), 'w')
+    if image_set not in classes:
+        class_count[image_set] = {}
     for image_id in image_ids:
-        name = convert_annotation(image_id,ann_path,label_path)
-        try:
-            list_file.write(f'{os.path.join(current_dir, dst_img_path,name)}\n')
-        except Exception as e:
-            print(f"error:{e}")
-            name = image_id + os.path.splitext(name)[1]
-            list_file.write(f'{os.path.join(current_dir, dst_img_path,name)}\n')
+        #print(f"image_id:{image_id}")
+        name = convert_annotation(image_id, ann_path, label_path,class_count[image_set])
+        #print(f"name:{name}")
+        if name == None:
+            name = image_id + '.jpg'
+        name = image_id + os.path.splitext(name)[1]
+        if os.path.splitext(name)[-1]=='.xml':
+            name = img_dict[image_id]
+        #print(f"name1:{name}")
+        list_file.write(f'{os.path.join(current_dir, dst_img_path, name)}\n')
     list_file.close()
+print(f"class_count:{class_count}")
